dae528446ec38ec0a4ac9265c835889d682e8bb5
Liza Khvesko <84271169+LizaKhvesko@users.noreply.github.com>
2021-05-20 19:25:33 +0300

    Solve first module (#2)
    * Added task Сделать первый символ заглавным
    
    * Implement capitalize function
    
    * Added task Проверка на спам
    
    * to uppercase first letter
    
    * Implement spam check function
    
    * Added task Усечение строки
    
    * Added task Допишите функцию валидации имени пользователя
    
    * Added task Вычислить факториал
    
    Co-authored-by: javascript-ru <47786167+jsru@users.noreply.github.com>
    Co-authored-by: javascriptru <67475495+jsru-1@users.noreply.github.com>
    Co-authored-by: komoskoann\\----------- <komoskoann@gmail.com>

---
+++ 1-module/3-task/index.js
@@ -0,0 +1,12 @@
+function ucFirst(str) {
+
+  if (str.length == 0) {
+      return "";
+  } else {
+      let firstLetter = str[0].toUpperCase();
+      let otherLetters = str.slice(1);
+      return firstLetter + otherLetters;
+  }
+}
+
+
